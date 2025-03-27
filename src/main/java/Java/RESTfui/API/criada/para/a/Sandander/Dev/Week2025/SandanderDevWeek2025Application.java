a169bc61cd115b4a333cbcfdadf2ccf735e24dfe
adriano dantas <adriianodantass17@gmail.com>
2025-03-27 16:53:14 -0300

    Initial commit

---
+++ src/main/java/Java/RESTfui/API/criada/para/a/Sandander/Dev/Week2025/SandanderDevWeek2025Application.java
@@ -0,0 +1,13 @@
+package Java.RESTfui.API.criada.para.a.Sandander.Dev.Week2025;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SandanderDevWeek2025Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SandanderDevWeek2025Application.class, args);
+	}
+
+}
---
+++ src/test/java/Java/RESTfui/API/criada/para/a/Sandander/Dev/Week2025/SandanderDevWeek2025ApplicationTests.java
@@ -0,0 +1,13 @@
+package Java.RESTfui.API.criada.para.a.Sandander.Dev.Week2025;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SandanderDevWeek2025ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
